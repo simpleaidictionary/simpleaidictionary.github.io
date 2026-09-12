a86c62706caa97421581115adbe58e0c0ad43d38
Ahmed <ahmed.akod@outlook.com>
2026-09-12 14:16:24 -0400

    fix: add fade to scroll

--- app/page.tsx
+++ app/page.tsx
@@ -24,21 +24,23 @@ export default function Home() {
           <h1 className="text-center px-4 text-2xl text-primary mb-2 font-playfair-display">
             Discover
           </h1>
-          <Card className="border border-border p-4 bg-secondary px-5 basis-72 overflow-scroll">
-            <ul className="list-none grid grid-cols-1 grid-flow-row px-2 gap-2 gap-x-8 text-base">
-              {getOutputTerms()
-                .slice(0, 12)
-                .map((term) => (
-                  <li className="text-center" key={term.name}>
-                    <Link
-                      className="capitalize text-primary  hover:underline decoration-2 "
-                      href={`/term/${term.url}`}
-                    >
-                      {term.name}
-                    </Link>
-                  </li>
-                ))}
-            </ul>
+          <Card className="border border-border p-1 px-4 bg-secondary basis-48 ">
+            <div className="overflow-scroll scroll-fade h-full">
+              <ul className="list-none grid grid-cols-1 grid-flow-row px-2 gap-2 gap-x-8 text-base">
+                {getOutputTerms()
+                  .slice(0, 12)
+                  .map((term) => (
+                    <li className="text-center" key={term.name}>
+                      <Link
+                        className="capitalize text-primary  hover:underline decoration-2 "
+                        href={`/term/${term.url}`}
+                      >
+                        {term.name}
+                      </Link>
+                    </li>
+                  ))}
+              </ul>
+            </div>
           </Card>
         </article>
       </main>
